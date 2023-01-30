2789f58d24dec783c04461199b3f7dc545d88428
karen-stepanyan <karen.stepanyan@protofire.io>
2023-01-30 21:04:50 +0400

    add total-burned endpoints

--- packages/sources/coinmetrics-test/src/index.ts
+++ packages/sources/coinmetrics-test/src/index.ts
@@ -1,12 +1,12 @@
 import { expose, ServerInstance } from '@chainlink/external-adapter-framework'
 import { Adapter } from '@chainlink/external-adapter-framework/adapter'
 import { customSettings } from './config'
-import { priceRouter, totalBurned } from './endpoint'
+import { burned, priceRouter, totalBurned } from './endpoint'
 
 export const adapter = new Adapter({
   defaultEndpoint: priceRouter.name,
   name: 'COINMETRICS',
-  endpoints: [priceRouter, totalBurned],
+  endpoints: [priceRouter, totalBurned, burned],
   customSettings,
   rateLimiting: {
     tiers: {
